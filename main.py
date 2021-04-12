3f5be43a00924be0946c815299556fd7c0dbb448
Ekky Sukmawati <>
2021-04-12 16:13:42 +0000

    Menmabahkan cara kedua

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ two_digit_number = input("Type a two digit number: ")
 ####################################
 #Write your code below this line 👇
 
+#cara 1
+#a= int(two_digit_number[0])
+#b = int(two_digit_number[1])
+# print(a+b)
 
-a= int(two_digit_number[0])
-b = int(two_digit_number[1])
-
-print(a+b)
+#cara 2
+add = int(two_digit_number[0]) + int(two_digit_number[1])
+print(add)
 
 
 
